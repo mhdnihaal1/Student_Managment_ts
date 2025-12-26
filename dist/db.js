93752a412fa97ebac7c52f34e4d0974ad6707e74
mhdnihaal1 <nihalmuhaednihal@gmail.com>
2025-12-26 17:54:03 +0530

    first commit

--- dist/db.js
+++ dist/db.js
@@ -6,7 +6,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const mongoose_1 = __importDefault(require("mongoose"));
 const connectDB = async () => {
     try {
-        const conn = await mongoose_1.default.connect('mongodb://127.0.0.1:27017/STUDENT_MANAGMENT');
+        const conn = await mongoose_1.default.connect('mongodb+srv://nihalmuhaednihal_db_user:Sxfg0fjhMEUttewc@cluster0.9oc3j0l.mongodb.net/students-managment?retryWrites=true&w=majority');
         console.log(`MongoDB connected: ${conn.connection.host}`);
         console.log('MongoDB connected');
     }
